1249c38e2158c1c9ce487c9deae8b518b44c1ba2
Galen Corey <galen.corey@gmail.com>
2016-09-12 18:24:26 -0400

    created signup conroller

--- browser/js/signup/signup.js
+++ browser/js/signup/signup.js
@@ -3,7 +3,26 @@ app.config(function ($stateProvider) {
     $stateProvider.state('signup', {
        url: '/signup' ,
        templateUrl: 'js/signup/signup.html',
-       controller: 'LogoutCtrl'
+       controller: 'SignupCtrl'
     });
+
+});
+
+app.controller('SignupCtrl', function ($scope, AuthService, $state) {
+
+    $scope.signup = {};
+    $scope.error = null;
+
+    $scope.sendSignup = function (signupInfo) {
+
+        $scope.error = null;
+
+        AuthService.signup(signupInfo).then(function () {
+            $state.go('home');
+        }).catch(function () {
+            $scope.error = 'Invalid login credentials.';
+        });
+
+    };
 
 });
