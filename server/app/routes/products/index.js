69860c165f85dace999f573062091e30a4371518
johanna <johanna08perez@gmail.com>
2016-09-14 14:39:12 -0400

    Products and Categories  (#23)
    * created products display page and route
    
    * creating seed file data
    
    * seeded categories for testing associations

--- server/app/routes/products/index.js
+++ server/app/routes/products/index.js
@@ -1,15 +1,24 @@
 'use strict';
 var router = require('express').Router(); // eslint-disable-line new-cap
-var Products = require('../../../db/models/products.js');
-var Category = require('../../../db/models/product_category.js');
-var Review = require('../../../db/models/review.js');
+const db = require('../../../db');
+const Products = db.model('product');
+const Category = db.model('category');
+const Review = db.model('review');
+var chalk = require('chalk');
 
 module.exports = router;
 
-router.get('/:productId', function(req, res, next) {
+router.get('/', function(req,res,next){
+  Products.findAll()
+  .then(function(response){
+    res.send(response);
+  })
+  .catch(next);
+});
+
+router.get('/item/:productId', function(req, res, next) {
   Products.findOne({where: {id: req.params.productId}, include: [Review]})
   .then(function(product) {
-
     if (!product) {
       var err = new Error('Product does not exist!');
       err.status = 404;
@@ -20,8 +29,16 @@ router.get('/:productId', function(req, res, next) {
   .catch(next);
 });
 
-router.get('/categories/:category', function(req, res, next) {
-  Category.findOne({where: {type_name: req.params.category}})
+router.get('/allcategories', function(req, res, next){
+  Category.findAll()
+  .then(function(response){
+    res.send(response);
+  })
+  .catch(next);
+});
+
+router.get('/categories/:categoryId', function(req, res, next) {
+  Category.findOne({where: {id: req.params.categoryId}})
   .then(function(category) {
     return category.getProducts();
   })
