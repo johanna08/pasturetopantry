c3ddacdcf289b6b79129cfa754093af89ce442ab
Ingrid Cronburg <ingrid257@gmail.com>
2016-09-16 16:26:38 -0400

    checkout view complete

--- browser/js/checkout/checkout.js
+++ browser/js/checkout/checkout.js
@@ -1,39 +1,7 @@
- 'use strict';
-app.controller('CartCtrl', function($scope, ProductFactory, $sessionStorage, products) {
-
-    $scope.products = products;
-
-    $scope.Range = function(start, end) {
-        var result = [];
-        for (var i = start; i <= end; i++) {
-            result.push(i);
-        }
-        return result;
-    };
-
-    $scope.getQuantity = function(id){
-    for (var i = 0; i < $sessionStorage.cart.length; i++) {
-            if ($sessionStorage.cart[i].id === id) return $sessionStorage.cart[i].quantity;
-        }
-    };
-
-    $scope.total = function() {
-      var total = 0;
-
-      for (var i = 0; i < $scope.products.length; i++) {
-        total += $scope.products[i].totalCost;
-      }
-
-      return total;
-    };
-
-});
-
-
 app.config(function($stateProvider) {
-    $stateProvider.state('cart', {
-        url: '/cart',
-        templateUrl: 'js/cart/cart.html',
+    $stateProvider.state('checkout', {
+        url: '/checkout',
+        templateUrl: 'js/checkout/checkout.html',
         controller: 'CartCtrl',
         resolve: {
             products: function($sessionStorage, ProductFactory) {
@@ -42,12 +10,15 @@ app.config(function($stateProvider) {
                     for (var i = 0; i < $sessionStorage.cart.length; i++) {
                         ProductFactory.getProduct($sessionStorage.cart[i].id)
                             .then(function(product) {
+
+
                                 for (var i = 0; i < products.length; i++) {
                                     if (products[i].id === product.id) {
                                         var inCart = true;
                                     }
                                 }
                                 if (!inCart) {
+                                    product.totalCost = $sessionStorage.cart[i].quantity * product.price;
                                     products.push(product);
                                 }
                             });
