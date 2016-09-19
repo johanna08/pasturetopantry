6a8f7b4f269c6df83a3d7911505be8e19e262327
Johanna Altmann <tintin301@gmail.com>
2016-09-19 18:24:01 -0400

    final edits for the day

--- browser/js/order/order.controller.js
+++ browser/js/order/order.controller.js
@@ -2,4 +2,6 @@
 
 app.controller('OrderCtrl', function($scope, $state, OrderFactory, order) {
   $scope.order = order;
+  console.log($scope.order.items);
+  $scope.orderTotal = OrderFactory.cartTotal($scope.order.items);
 });
