58837f6a84a6b842463da011429770f5531c8356
Johanna Altmann <tintin301@gmail.com>
2016-09-20 17:50:05 -0400

    can add new category when creating product

--- browser/js/admin/admin.controller.js
+++ browser/js/admin/admin.controller.js
@@ -8,8 +8,25 @@ app.controller('AdminCtrl', function($scope, AdminFactory, Products, categories,
   $scope.deleteProduct = AdminFactory.deleteProduct;
 
   $scope.addProduct = function(product) {
-    AdminFactory.addProduct(product, $scope.categories, $scope.categorySelection)
-    .catch($log.error);
+    //if the we have added new categories with the new product we are creating,
+    //get the names and add them to the category database
+    //then add the names to the categories array & $scope.categories selection object
+    let selectedCategories = Object.keys($scope.categorySelection);
+    let newCats;
+    if ($scope.newCategories.length) newCats = $scope.newCategories.match(/[a-zA-Z]+/g);
+    let categoryPromises = newCats.map(function(category) {
+      return AdminFactory.addCategory({type_name: category})
+      .then(function(category) {
+        console.log(category);
+        $scope.categories.push(category.type_name);
+        return selectedCategories.push(category.type_name);
+      })
+    });
+    Promise.resolve(categoryPromises)
+    .then(function(){
+      AdminFactory.addProduct(product, $scope.categories, selectedCategories)
+      .catch($log.error);
+    })
   }
 
   $scope.deleteProduct = AdminFactory.deleteProduct;
--- browser/js/admin/admin.factory.js
+++ browser/js/admin/admin.factory.js
@@ -8,39 +8,32 @@ app.factory('AdminFactory', function($http, $log) {
   return {
     //product is from controller scope added by ng-model in form -> product obj
     addProduct: function(product, categories, selectCategories) {
-      let selectedCategories = Object.keys(selectCategories);
       let addCategories = categories.reduce(function(arr, obj) {
-        if (selectedCategories.includes(obj.type_name)) arr.push(obj.id);
+        if (selectCategories.includes(obj.type_name)) arr.push(obj.id);
         return arr;
       }, []);
       return $http.post('/api/products', {product: product, categories: addCategories})
-      .then(sendResponse)
-      .catch($log.error);
-
+      .then(sendResponse);
     },
     deleteProduct: function(id) {
       return $http.delete('/api/products/' + id)
-      .then(sendResponse)
-      .catch($log.error);
+      .then(sendResponse);
 
     },
     getOrders: function() {
       return $http.get('api/reviews')
-      .then(sendResponse)
-      .catch($log.error);
+      .then(sendResponse);
     },
     addCategory: function(category) {
       return $http.post('/api/categories', category)
-      .then(sendReponse)
-      .catch($log.error);
+      .then(sendResponse);
     },
     deleteCategory: function(id) {
       return $http.delete('/api/categories' + id)
-      .then(sendResponse)
-      .catch($log.error);
+      .then(sendResponse);
     },
     displayProductCategories: function(product) {
-      return product.categories.map(category => category.type_name).join(', ')
+      return product.categories.map(category => category.type_name).join(', ');
     }
   }
 });
