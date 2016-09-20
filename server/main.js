031e8f56e4371cf73454698b04bc56b9c3487616
johanna <johanna08perez@gmail.com>
2016-09-20 17:24:25 -0400

    Deployment (#60)
    * new keys
    
    * server

--- server/main.js
+++ server/main.js
@@ -9,18 +9,51 @@ var secureConfig = {
   key: fs.readFileSync('./key.pem'),
   cert: fs.readFileSync('./cert.pem')
 };
+
+if(process.env.NODE_ENV === "production" || process.env.NODE_ENV === "testing"){
 // Create a node server instance! cOoL!
-var server = require('https').createServer(secureConfig).listen(port, function(err){
-    if (err) throw err;
+    var server = require('https').createServer(secureConfig).listen(port, function(err){
+        if (err) throw err;
+        db.sync()
+        .then(createApplication)
+        .catch(function (err) {
+        console.error(chalk.red(err.stack));
+        });
+    });
+    var createApplication = function () {
+        var app = require('./app')(db);
+        server.on('request', app); // Attach the Express application.
+        require('./io')(server);   // Attach socket.io.
+        console.log('made app')
+    };
+}
+
+//if in deployment mode cannot run https with heroku
+else{
+
+    // Create a node server instance! cOoL!
+    var server = require('http').createServer();
+
+    var createApplication = function () {
+        var app = require('./app')(db);
+        server.on('request', app); // Attach the Express application.
+        require('./io')(server);   // Attach socket.io.
+    };
+
+    var startServer = function () {
+
+        var PORT = process.env.PORT || 1337;
+
+        server.listen(PORT, function () {
+            console.log(chalk.blue('Server started on port', chalk.magenta(PORT)));
+        });
+
+    };
+
     db.sync()
     .then(createApplication)
+    .then(startServer)
     .catch(function (err) {
-    console.error(chalk.red(err.stack));
+        console.error(chalk.red(err.stack));
     });
-});
-var createApplication = function () {
-    var app = require('./app')(db);
-    server.on('request', app); // Attach the Express application.
-    require('./io')(server);   // Attach socket.io.
-    console.log('made app')
-};
+}
