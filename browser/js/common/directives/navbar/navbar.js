da906f12238ff0219f4499e4a2b44a9c93adf176
Ingrid Cronburg <ingrid257@gmail.com>
2016-09-20 16:24:17 -0400

    homepage additions

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -8,7 +8,6 @@ app.directive('navbar', function($rootScope, AuthService, AUTH_EVENTS, $state) {
 
             scope.items = [
                 { label: 'Products', state: 'products' },
-                { label: 'Home', state: 'home' },
                 { label: 'About', state: 'about' },
                 { label: 'Documentation', state: 'docs' },
                 { label: 'Members Only', state: 'membersOnly', auth: true }
