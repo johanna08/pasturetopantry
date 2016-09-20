d19e2690462dfb0920e788f99621ab27f9dabc10
josie11 <tintin301@gmail.com>
2016-09-20 18:31:04 -0400

    Adminhomepage johanna (#62)
    * admin page features
    
    * can add new category when creating product
    
    * edit categories page works

--- browser/js/admin/admin.controller.js
+++ browser/js/admin/admin.controller.js
@@ -1,20 +1,49 @@
 'use strict';
 
-app.controller('AdminCtrl', function($scope, $state, AdminFactory, Products, categories, $log, products) {
+app.controller('AdminCtrl', function($scope, AdminFactory, Products, categories, $log, products,$state) {
   $scope.categorySelection = {};
   $scope.categories = categories;
+  $scope.products = products;
+  $scope.displayProductCategories = AdminFactory.displayProductCategories;
+  $scope.deleteProduct = AdminFactory.deleteProduct;
+  $scope.addCategory = AdminFactory.addCategory;
 
-  $scope.addProduct = function(product) {
-    let selectedCategories = Object.keys($scope.categorySelection)
-    let categories = $scope.categories.reduce(function(arr, obj) {
-      if (selectedCategories.includes(obj.type_name)) arr.push(obj.id);
-      return arr;
-    }, []);
-    AdminFactory.addProduct({product: product, categories: categories})
-    .then(function(product) {
-      $state.go('product', { id: product.id });
+  $scope.deleteCategory = function(){
+    AdminFactory.deleteCategory($scope.categorySelected.id)
+    .then(function(){
+      $state.reload();
     })
     .catch($log.error);
   }
 
+  $scope.editCategory = function() {
+    AdminFactory.editCategory($scope.categorySelected.id, $scope.newCategoryName)
+    .then(function(){
+      $state.reload();
+    })
+    .catch($log.error);
+  }
+
+  $scope.addProduct = function(product) {
+    //if the we have added new categories with the new product we are creating,
+    //get the names and add them to the category database
+    //then add the names to the categories array & $scope.categories selection object
+    //not great, should be on factory, no time to refactor
+    let selectedCategories = Object.keys($scope.categorySelection);
+    let newCats;
+    if ($scope.newCategories.length) newCats = $scope.newCategories.match(/[a-zA-Z]+/g);
+    let categoryPromises = newCats.map(function(category) {
+      return AdminFactory.addCategory({type_name: category})
+      .then(function(category) {
+        console.log(category);
+        $scope.categories.push(category.type_name);
+        return selectedCategories.push(category.type_name);
+      })
+    });
+    Promise.resolve(categoryPromises)
+    .then(function(){
+      AdminFactory.addProduct(product, $scope.categories, selectedCategories)
+      .catch($log.error);
+    })
+  }
 })
--- browser/js/admin/admin.factory.js
+++ browser/js/admin/admin.factory.js
@@ -7,32 +7,37 @@ app.factory('AdminFactory', function($http, $log) {
 
   return {
     //product is from controller scope added by ng-model in form -> product obj
-    addProduct: function(product) {
-      return $http.post('/api/products', product)
-      .then(sendResponse)
-      .catch($log.error);
-
+    addProduct: function(product, categories, selectCategories) {
+      let addCategories = categories.reduce(function(arr, obj) {
+        if (selectCategories.includes(obj.type_name)) arr.push(obj.id);
+        return arr;
+      }, []);
+      return $http.post('/api/products', {product: product, categories: addCategories})
+      .then(sendResponse);
     },
     deleteProduct: function(id) {
       return $http.delete('/api/products/' + id)
-      .then(sendResponse)
-      .catch($log.error);
+      .then(sendResponse);
 
     },
     getOrders: function() {
       return $http.get('api/reviews')
-      .then(sendResponse)
-      .catch($log.error);
+      .then(sendResponse);
     },
     addCategory: function(category) {
       return $http.post('/api/categories', category)
-      .then(sendReponse)
-      .catch($log.error);
+      .then(sendResponse);
     },
     deleteCategory: function(id) {
-      return $http.delete('/api/categories' + id)
-      .then(sendResponse)
-      .catch($log.error);
+      return $http.delete('/api/categories/' + id)
+      .then(sendResponse);
+    },
+    editCategory: function(categoryId, newCategoryName) {
+      return $http.put('/api/categories', {id: categoryId, type_name: newCategoryName})
+      .then(sendResponse);
+    },
+    displayProductCategories: function(product) {
+      return product.categories.map(category => category.type_name).join(', ');
     }
   }
 });
