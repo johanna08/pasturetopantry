84831b9e5bcf1f075a02edbf359fe2d8d4c72328
johanna <johanna08perez@gmail.com>
2016-09-20 15:30:06 -0400

    Stripeaddtodb (#55)
    * new keys
    
    * added order details from stripe to db
    
    * cleaned up backend routes for order
    
    * Stripe fully functional
    
    * checkout fully functional with stripe

--- server/db/models/order-details.js
+++ server/db/models/order-details.js
@@ -8,17 +8,20 @@ var db = require('../_db');
 module.exports = db.define('orderDetails', {
     name: {
         type: Sequelize.STRING,
-        allowNull: false
+        // allowNull: false
     },
     email: {
         type: Sequelize.STRING,
-        validate: {
-            isEmail: true
-        },
-        allowNull: false
+        // validate: {
+        //     isEmail: true
+        // },
+        // allowNull: false
     },
     address: {
         type: Sequelize.STRING,
-        allowNull: false
+        // allowNull: false
+    },
+    token: {
+        type: Sequelize.STRING
     }
 });
